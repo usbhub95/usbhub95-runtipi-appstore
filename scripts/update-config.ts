1cf51496369a459518df1a0250ef1a0aed6122a1
Cooper Lockrey <43819486+usbhub95@users.noreply.github.com>
2025-09-02 04:08:33 +1000

    Removed the existing template app and relicensed the project.

--- scripts/update-config.ts
+++ scripts/update-config.ts
@@ -1,3 +1,20 @@
+// Copyright (C) 2025 Nicolas Meienberger
+// 
+// This file is part of usbhub95-runtipi-appstore.
+// 
+// usbhub95-runtipi-appstore is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// usbhub95-runtipi-appstore is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with usbhub95-runtipi-appstore.  If not, see <https://www.gnu.org/licenses/>.
+
 import path from "node:path";
 import fs from "fs/promises";
 
